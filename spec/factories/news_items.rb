75837293b740376bd645bb6a577117da5a6d7c20
Rong Sun <jessie_sr@berkeley.edu>
2023-11-30 01:30:18 +0000

    Updated spec/factories with news_items.rb

---
+++ spec/factories/news_items.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :news_item do
+    title { "Sample News Title" }
+    description { "Sample news description." }
+    link { "http://example.com/news" }
+    issue { "Climate Change" }  # Add a default issue
+    representative { create(:representative) }  # Assuming you have a representative factory
+
+    # Include other fields as necessary
+  end
+end
