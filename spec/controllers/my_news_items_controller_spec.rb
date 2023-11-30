0167b3868f30bc7c3c8abef58d90478cb614469e
Rong Sun <jessie_sr@berkeley.edu>
2023-11-30 01:43:49 +0000

    Modified rspec tests for my_news_items

--- spec/controllers/my_news_items_controller_spec.rb
+++ spec/controllers/my_news_items_controller_spec.rb
@@ -5,31 +5,37 @@ require 'rails_helper'
 RSpec.describe MyNewsItemsController, type: :controller do
   # Setup, authentication, etc.
 
+  let(:representative) { create(:representative) }
+
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'creates a new news item with an issue' do
         expect do
-          post :create, params: { news_item: attributes_for(:news_item, issue: 'Climate Change') }
+          post :create, params: { 
+                          representative_id: representative.id,
+                          news_item: attributes_for(:news_item, issue: 'Climate Change') 
+                        }
         end.to change(NewsItem, :count).by(1)
         expect(NewsItem.last.issue).to eq('Climate Change')
       end
     end
-
-    # Other contexts for invalid attributes, etc.
   end
 
-  describe 'PUT #update' do
-    let!(:news_item) { create(:news_item, issue: 'Immigration') }
+  let(:representative) { create(:representative) }
+  let!(:news_item) { create(:news_item, issue: 'Immigration', representative: representative) }
 
+  describe 'PUT #update' do
     context 'with valid attributes' do
       it 'updates the issue of the news item' do
-        put :update, params: { id: news_item.id, news_item: { issue: 'Tax Reform' } }
+        put :update, params: { 
+                      id: news_item.id, 
+                      representative_id: representative.id,
+                      news_item: { issue: 'Tax Reform' } 
+                    }
         news_item.reload
         expect(news_item.issue).to eq('Tax Reform')
       end
     end
-
-    # Other contexts for invalid attributes, etc.
   end
 
   # Other tests...
