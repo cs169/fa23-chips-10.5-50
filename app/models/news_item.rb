69eb48e007b5d6ab17c795e889faf8e4090ebfcf
Rong Sun <jessie_sr@berkeley.edu>
2023-11-30 01:28:27 +0000

    Added rspec tests for news_item.rb and my_news_items_controller.rb

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -4,6 +4,8 @@ class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
 
+  validates :issue, inclusion: { in: ["Free Speech", "Immigration", "Terrorism", "Social Security and Medicare", "Abortion", "Student Loans", "Gun Control", "Unemployment", "Climate Change", "Homelessness", "Racism", "Tax Reform", "Net Neutrality", "Religious Freedom", "Border Security", "Minimum Wage", "Equal Pay"], message: "%{value} is not a valid issue" }
+  
   def self.find_for(representative_id)
     NewsItem.find_by(
       representative_id: representative_id
---
+++ spec/models/news_item_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe NewsItem, type: :model do
+  # Assuming you have factories set up for your models
+  let(:news_item) { build(:news_item) }
+
+  describe 'validations' do
+    it 'is valid with a valid issue' do
+      valid_issues = ['Free Speech', 'Immigration', 'Terrorism', 'Social Security and Medicare', 'Abortion',
+                      'Student Loans', 'Gun Control', 'Unemployment', 'Climate Change', 'Homelessness', 'Racism', 'Tax Reform', 'Net Neutrality', 'Religious Freedom', 'Border Security', 'Minimum Wage', 'Equal Pay']
+      valid_issues.each do |issue|
+        news_item.issue = issue
+        expect(news_item).to be_valid
+      end
+    end
+
+    it 'is not valid with an invalid issue' do
+      news_item.issue = 'Invalid Issue'
+      expect(news_item).not_to be_valid
+      expect(news_item.errors[:issue]).to include('Invalid Issue is not a valid issue')
+    end
+  end
+
+  # Other tests for your model...
+end
