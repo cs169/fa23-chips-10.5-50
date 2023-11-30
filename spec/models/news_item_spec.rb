e74e9c05a484f98ada896847b259c429079408ca
Rong Sun <jessie_sr@berkeley.edu>
2023-11-30 01:35:30 +0000

    Updated spec/factories with representatives.rb

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 
 RSpec.describe NewsItem, type: :model do
   # Assuming you have factories set up for your models
-  let(:news_item) { build(:news_item) }
+  let(:representative) { create(:representative) }
+  let(:news_item) { build(:news_item, representative: representative) }
 
   describe 'validations' do
     it 'is valid with a valid issue' do
